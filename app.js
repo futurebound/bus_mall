4e1e5911a86f5026d819fd3c14790f74b30caedf
Mitchell M <mitchellm1618@gmail.com>
2017-11-01 08:55:52 -0700

    added chart

--- app.js
+++ app.js
@@ -125,3 +125,25 @@ function displayImages() {
 }
 
 displayImages();
+
+
+// chart
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'pie',
+
+    // The data for our dataset
+    data: {
+        labels: [],
+        datasets: [{
+            label: "Statistics",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
